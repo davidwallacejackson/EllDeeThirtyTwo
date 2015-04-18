efb965f82497e45dcded37dd93327e1ab9afdc7b
David Jackson <altmail@jackson-wallace.net>
2015-04-17 23:06:51 -0500

    Player can move around the level.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D body;
+    IInput input;
+
+    public float acceleration;
+    public float maxSpeed;
+
+    // Use this for initialization
+	void Start () {
+        input = GetComponent<PlayerInput>();
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 impulse = input.GetMoveAxis();
+
+        if (body.velocity.magnitude < maxSpeed)
+        {
+            impulse = impulse.normalized * acceleration * Time.deltaTime;
+
+            body.AddForce(impulse);
+        }
+
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour, IInput {
+
+    Vector2 currentMove;
+
+	// Update is called once per frame
+	void Update () {
+        currentMove = Vector3.zero;
+
+        currentMove.x = Input.GetAxis("Horizontal");
+        currentMove.y = Input.GetAxis("Vertical");
+	}
+
+    public Vector2 GetMoveAxis()
+    {
+        return currentMove;
+    }
+}
