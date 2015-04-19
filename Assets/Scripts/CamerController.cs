623fa4e8d003619ff9f4200d52c8b66e7346dd0b
David Jackson <altmail@jackson-wallace.net>
2015-04-19 16:00:29 -0500

    Implemented simple camera follow.

---
+++ Assets/Scripts/CamerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LD32
+{
+    public class CamerController : BaseBehaviour
+    {
+        Camera camera;
+        PlayerController player;
+        #region Unity Hooks
+        public override void Awake()
+        {
+            base.Awake();
+            camera = GetComponent<Camera>();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            player = FindObjectOfType<PlayerController>();
+
+        }
+
+        public void Update()
+        {
+            transform.position = new Vector3(
+                player.transform.position.x,
+                player.transform.position.y,
+                this.transform.position.z);
+        }
+        #endregion
+
+        #region Public API
+        #endregion
+
+        #region Internal Methods
+        #endregion
+
+        #region Event Callbacks
+        #endregion
+    }
+
+}
